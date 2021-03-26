30dd65ea062af047ca85a00d53541f93c8620ba1
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-03-25 23:10:27 -0400

    Update README.asm

--- README.asm
+++ README.asm
@@ -2,3 +2,7 @@ Credits:
 
 Programming: CodeSalvageON
 Icon and Ideas: movesen#1122
+
+Info about the game/community website:
+
+Keep in mind that this is a project originally abandoned by me and it has 800+ bugs and issues
